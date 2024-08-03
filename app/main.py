7b464f020ffe3bb76a69765783c2f763807229be
JunoZeno <juliohernandez235@gmail.com>
2024-08-03 18:42:39 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -6,13 +6,20 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
-
     print("Server started at localhost:4221")
 
     while True:
-        # Return 200 response with HTTP/1.1 200 OK\r\n\r\n
-        server_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        # Accept a new connection
+        client_socket, client_address = server_socket.accept()
+        print(f"client_socket: {client_socket}")
+        print(f"Connection from {client_address}")
+
+        # Send HTTP response
+        response = "HTTP/1.1 200 OK\r\n\r\n"
+        client_socket.sendall(response.encode('utf-8'))
+
+        # Close the client socket
+        client_socket.close()
 
 
 if __name__ == "__main__":
