9fc994cf8ce80e97326b5019d94a4b967d6c4547
JunoZeno <juliohernandez235@gmail.com>
2024-08-04 21:59:01 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket
+from http import HTTPStatus
 
 
 def main():
@@ -11,11 +12,31 @@ def main():
     while True:
         # Accept a new connection
         client_socket, client_address = server_socket.accept()
-        print(f"client_socket: {client_socket}")
+        # print(f"client_socket: {client_socket}") # TODO REMOVE
         print(f"Connection from {client_address}")
 
-        # Send HTTP response
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+        # Receive the HTTP request
+        request = client_socket.recv(1024).decode('utf-8')
+        print(f"Received request: {request}")
+
+        # Extract the request target
+        request_line = request.split('\r\n')[0]
+        request_target = request_line.split(' ')[1]
+        print(f"Request target: {request_target}")
+
+        # Extract the URL
+        url = request_target
+        print(f"URL: {url}")
+
+        if url == "/":
+            # Send 200 HTTP response
+            response = f"HTTP/1.1 {HTTPStatus.OK.value} {HTTPStatus.OK.phrase}\r\n\r\n"
+        else:
+            # Send 400 HTTP response
+            response = f"HTTP/1.1 {HTTPStatus.NOT_FOUND.value} {HTTPStatus.NOT_FOUND.phrase}\r\n\r\n"
+
+        print(f"Sending response: {response}")
+
         client_socket.sendall(response.encode('utf-8'))
 
         # Close the client socket
